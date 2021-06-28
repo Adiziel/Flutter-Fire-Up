e5f77d57fbbd561eff1de886607054af62fa5847
Aditya Pathak <aditya.pathak.plus@gmail.com>
2021-06-28 16:03:39 +0530

    fourteenth commit

--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -4,7 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 class MyThemes {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
       primarySwatch: Colors.deepPurple,
-      fontFamily: GoogleFonts.lato().fontFamily,
+      fontFamily: GoogleFonts.poppins().fontFamily,
       appBarTheme: AppBarTheme(
         color: Colors.white,
         elevation: 0.0,
@@ -14,4 +14,8 @@ class MyThemes {
 
   static ThemeData darkTheme(BuildContext context) =>
       ThemeData(brightness: Brightness.dark);
+
+  //Colors
+  static Color doomsickColor = Color(0xFFF3EFEFf);
+  static Color darkbluish = Color(0xff403b58);
 }
